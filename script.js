8ef5129c9c7318b0b0586dfbd2df040700c6c54d
MihajlovicSS <mihajlovicsinisa1456780@gmail.com>
2023-03-10 17:22:43 +0200

    Version two

--- script.js
+++ script.js
@@ -6,17 +6,23 @@ const button = document.querySelector('#msgButton')
 addToListOnClick ()
 
 function addToListOnClick (){
+    list.textContent = 'TODO List'
+    list.className = 'list-title'
     button.addEventListener('click', onButtonClick)
 }
 function onButtonClick (){
     if (validation(input.value)){
         const li = document.createElement('li')
+        li.className = 'list-item'
         li.textContent = input.value
         list.append(li)
     }
     input.value = ''
 }
 function validation (value){
-    if (value.trim() === '') return false
+    if (value.trim() === '') {
+        alert('The field must be not empty!')
+        return false
+    }
     else return true
 }
